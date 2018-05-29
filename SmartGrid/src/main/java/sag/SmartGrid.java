8a37208a308284067c60e1e85c51812938978d9f
Magdalena Grzesiak <maggda.grzesiak@gmail.com>
2018-05-29 02:37:05 +0200

    Move actor creation to new util class, ActorCreator

--- SmartGrid/src/main/java/sag/SmartGrid.java
+++ SmartGrid/src/main/java/sag/SmartGrid.java
@@ -2,156 +2,16 @@ package sag;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import sag.actors.Client;
-import sag.actors.Network;
-import sag.actors.Supervisor;
+import sag.utils.ActorCreator;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
 
-public class SmartGrid
-{
-    private static ActorSystem system;
-    private static List<ActorRef> supervisors, networks, clients;
-
-
-    public static void main( String[] args )
-    {
-        system = ActorSystem.create("SmartGridSystem");
-
-        SmartGrid sg = new SmartGrid();
-        sg.getActors();
-
-        /*
-        try {
-        	final ActorRef supervisor = system.actorOf(Supervisor.props(), "supervisor");
-        	final ActorRef network = system.actorOf(Network.props(supervisor), "network");
-            final ActorRef client1 = system.actorOf(Client.props(0.0, 100.0, 1, 1, supervisor, network), "client1");
-        	final ActorRef client2 = system.actorOf(Client.props(50.0, 0.0, 2, 1, supervisor, network), "client2");
-            final ActorRef client3 = system.actorOf(Client.props(25.0, 0.0, 3, 1, supervisor, network), "client3");
-            final ActorRef client4 = system.actorOf(Client.props(17.0, 0.0, 4, 1, supervisor, network), "client4");
-            final ActorRef client5 = system.actorOf(Client.props(8.0, 0.0, 5, 1, supervisor, network), "client5");
-        	
-        	
-        	//System.out.println(">>> Press ENTER to exit <<<");
-        	System.in.read();
-        	
-        } catch (IOException ioe) {
-        } finally {
-          system.terminate();
-        }
-        */
-        
+public class SmartGrid {
+    public static void main( String[] args ) {
+        final ActorSystem system = ActorSystem.create("SmartGridSystem");
+        final ActorCreator creator = new ActorCreator(system);
+        final Map<String, ActorRef> supervisors = creator.createSupervisors("supervisors.txt");
+        final Map<String, ActorRef>networks = creator.createNetworks("networks.txt", supervisors);
+        creator.createClients("clients.txt", supervisors, networks);
     }
-
-    private void getActors(){
-        ClassLoader classLoader = getClass().getClassLoader();
-
-        // get supervisors
-        File supervisorsFile = new File(classLoader.getResource("supervisors.txt").getFile());
-        supervisors = new ArrayList<ActorRef>();
-        try (Scanner scanner = new Scanner(supervisorsFile)) {
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                supervisors.add(system.actorOf(Supervisor.props(), line));
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        /*
-        System.out.println("Moi nadzorcy");
-        for(ActorRef a : supervisors){
-            System.out.println(a.toString());
-            String s = a.toString();
-            System.out.println(s.substring(s.lastIndexOf("/") + 1, s.indexOf("#")));
-        }
-        */
-
-        // get networks
-        File networksFile = new File(classLoader.getResource("networks.txt").getFile());
-        networks = new ArrayList<ActorRef>();
-        try (Scanner scanner = new Scanner(networksFile)) {
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] param = line.split(" ");
-
-                for(ActorRef sup : supervisors){
-                    String s = sup.toString();
-                    if(s.substring(s.lastIndexOf("/") + 1, s.indexOf("#")).equals(param[1])){
-                        networks.add(system.actorOf(Network.props(sup), param[0]));
-                    }
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        /*
-        System.out.println("Moje sieci:");
-        for(ActorRef net : networks){
-            System.out.println(net.toString());
-            String s = net.toString();
-            System.out.println(s.substring(s.lastIndexOf("/") + 1, s.indexOf("#")));
-        }
-        */
-
-        //get clients
-        File clientsFile = new File(classLoader.getResource("clients.txt").getFile());
-        clients = new ArrayList<ActorRef>();
-        try (Scanner scanner = new Scanner(clientsFile)) {
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] param = line.split(" ");
-
-                int i=0;
-                for(ActorRef sup : supervisors){
-                    String s = sup.toString();
-                    if(s.substring(s.lastIndexOf("/") + 1, s.indexOf("#")).equals(param[5])){
-                        break;
-                    } else {
-                        ++i;
-                    }
-                }
-
-                int j = 0;
-                for(ActorRef net : networks){
-                    String s = net.toString();
-                    if(s.substring(s.lastIndexOf("/") + 1, s.indexOf("#")).equals(param[6])){
-                        break;
-                    }else{
-                        ++j;
-                    }
-                }
-
-                clients.add(system.actorOf(Client.props(Double.parseDouble(param[1]), Double.parseDouble(param[2]), Double.parseDouble(param[3]), Double.parseDouble(param[4]), supervisors.get(i), networks.get(j)), param[0]));
-
-            }
-
-            scanner.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        /*
-        System.out.println("Moi klienci:");
-        for(ActorRef c : clients){
-            System.out.println(c.toString());
-            String s = c.toString();
-            System.out.println(s.substring(s.lastIndexOf("/") + 1, s.indexOf("#")));
-        }
-        */
-
-    }
-
-
 }
---
+++ SmartGrid/src/main/java/sag/utils/ActorCreator.java
@@ -0,0 +1,107 @@
+package sag.utils;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.japi.Pair;
+import sag.actors.Client;
+import sag.actors.Network;
+import sag.actors.Supervisor;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Klasa pomocnicza pozwalająca na wczytywanie z plików aktorów konkretnego typu.
+ */
+public class ActorCreator {
+    private ActorSystem actorSystem;
+    private ClassLoader classLoader = ActorCreator.class.getClassLoader();
+
+    public ActorCreator(final ActorSystem system) {
+        this.actorSystem = system;
+    }
+
+    /**
+     * Utworzenie map z nazw aktorów do aktorów na podstawie pliku o wskazanej nazwie.
+     * @param fileName Nazwa pliku przechowująca nazwy nadzorców.
+     * @return Mapa nazwa-nadzorca.
+     */
+    public Map<String, ActorRef> createSupervisors(final String fileName) {
+        Stream<String> lines = resourceLines(fileName);
+        return lines
+            .map(name -> new Pair<>(name, actorSystem.actorOf(Supervisor.props(), name)))
+            .collect(Collectors.toMap(Pair::first, Pair::second));
+    }
+
+    /**
+     * Utworzenie map z nazw sieci do sieci na podstawie pliku o wskazanej nazwie.
+     * @param fileName Nazwa pliku przechowująca nazwy sieci oraz przypisanych im nadzorców.
+     * @param supervisors Mapa nazwa-nadzorca.
+     * @return Mapa nazwa-sieć.
+     */
+    public Map<String, ActorRef> createNetworks(final String fileName, final Map<String, ActorRef> supervisors) {
+        Stream<String> lines = resourceLines(fileName);
+         return lines
+            .map(line -> line.split(" "))
+            .map(pair -> {
+                String networkName = pair[0];
+                String supervisorName = pair[1];
+                return new Pair<>(
+                    networkName,
+                    actorSystem.actorOf(Network.props(supervisors.get(supervisorName)), networkName)
+                );
+            })
+            .collect(Collectors.toMap(Pair::first, Pair::second));
+    }
+
+    /**
+     * Utworzenie map z nazw klienta do klienta na podstawie pliku o wskazanej nazwie.
+     * @param fileName Nazwa pliku przechowująca nazwy klientów oraz przypisane im
+     *                 zapotrzebowanie na medium, produkcję medium, położenie reprezentowane
+     *                 przez dwie współrzędne, nadzorców oraz sieci.
+     * @param supervisors Mapa nazwa-nadzorca.
+     * @param networks Mapa nazwa-sieć.
+     * @return Mapa nazwa-klient.
+     */
+    public Map<String, ActorRef> createClients(final String fileName,
+                                               final Map<String, ActorRef> supervisors,
+                                               final Map<String, ActorRef> networks) {
+        Stream<String> lines = resourceLines(fileName);
+        return lines
+            .map(line -> line.split(" "))
+            .map(parts -> {
+                String clientName = parts[0];
+                Double demand = Double.parseDouble(parts[1]);
+                Double production = Double.parseDouble(parts[2]);
+                Double xCoord = Double.parseDouble(parts[3]);
+                Double yCoord = Double.parseDouble(parts[4]);
+                String supervisorName = parts[5];
+                String networkName = parts[6];
+                return new Pair<>(
+                    clientName,
+                    actorSystem.actorOf(
+                        Client.props(
+                                demand,
+                                production,
+                                xCoord,
+                                yCoord,
+                                supervisors.get(supervisorName),
+                                networks.get(networkName)
+                        ), clientName)
+                );
+            })
+            .collect(Collectors.toMap(Pair::first, Pair::second));
+    }
+
+    private Stream<String> resourceLines(final String fileName) {
+        return new BufferedReader(
+            new InputStreamReader(
+                classLoader.getResourceAsStream(fileName)
+            )
+        )
+        .lines();
+    }
+}
